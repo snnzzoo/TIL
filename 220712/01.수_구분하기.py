6c43880af8650d36afc08be59a14a96d1772edac
snnzzoo <tjss1580@gmail.com>
2022-07-13 03:59:55 +0900

    Python_Day_2

---
+++ 220712/01.수_구분하기.py
@@ -0,0 +1,6 @@
+n = int(input())
+if n % 6 == 0:
+    print('참')
+else:
+    print('거짓')
+    
---
+++ 220712/02.길이_구하기.py
@@ -0,0 +1,18 @@
+word = 'happy!'
+z = 0
+for x in word:
+    z += 1
+print(z)
+
+
+# 문자열 word의 길이를 출력하는 코드를 각각
+# word = 'happy!'
+# 6
+word = 'happy!'
+count = 0
+
+# 모든 문자를 돌면서
+for char in word:
+# 1씩 증가시킨다.
+    count = count + 1
+print(count)
