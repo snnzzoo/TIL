1fef09192af89710cea3b1d3c2fb738ee370bddd
snnzzoo <tjss1580@gmail.com>
2022-07-14 03:06:15 +0900

    문제풀이

--- 220712/Python 실습문제/06.최댓값_구하기.py
+++ 220712/Python 실습문제/06.최댓값_구하기.py
@@ -1,13 +1,15 @@
 # 최댓값 구하기
-numbers = [-10, -100, -30]
-max_num = numbers[0] # 최댓값 설정시 값을 초기 ~~로 하기
+numbers = [0, 20, 100]
+max_num = numbers[0] # 통 안의 숫자 활용하기
+# 최솟값 설정
+# max_num = 0
 # max_num = -1000000
-# max_num = float("-inf")
+# max_num = float("-inf") # 음의 무한대
 
 # 1. 반복
 for n in numbers:
     # print(n)
-    # 2. 만약, max값이 n보다 작으면 바꾼다.
+    # 2. 만약, max값이 n보다 작으면 n으로 값을 바꾼다.
     if max_num < n:
         # print('왔냐?')
         max_num = n
@@ -15,9 +17,10 @@ print(max_num)
 
 
 numbers = [-10, -100, -30]
-max_num = numbers[0]
+max_num = float('-inf')
 
 for i in numbers:
+    print(i)
     if max_num < i:
         max_num = i
 print(max_num)
--- 220712/Python 실습문제/08.두번째로큰수_구하기.py
+++ 220712/Python 실습문제/08.두번째로큰수_구하기.py
@@ -1,16 +1,29 @@
-from tkinter import N
-
-
-numbers = [0, 20, 100, 40]
-max_number = numbers[0]
-# max_number = float("-inf")
-second_number = numbers[0]
-# second_number = float("-inf")
+numbers = [-10, -100, -30]
+max_number = float('-inf')
+second_number = float('-inf')
 
 # 1. 전체 숫자를 반복
 for n in numbers:
-    # 만약에, n이 최대보다 크다면
+    # 만약에, n이 최댓값보다 크다면
     if max_number < n:
-        # 최대값이었던 것이 두 번쨰로 큰 수
+        # 최댓값이었던 것이 두 번째로 큰 수가 됨
         second_number = max_number
+        # 최댓값은 n이 됨
         max_number = n
+    # elif second_number < n < max_number:
+    elif second_number < n and n < max_number:
+        second_number = n    
+print(second_number)
+
+
+numbers = [-10, -100, -30]
+max_num = numbers[0]
+second = numbers[0]
+
+for i in numbers:
+    if max_num < i:
+        second = max_num
+        max_num = i
+    elif second < i and i < max_num:
+        second = i
+print(second)
