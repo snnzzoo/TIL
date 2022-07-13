b810588e7cad41e03185032e2e42e55986a9069f
snnzzoo <tjss1580@gmail.com>
2022-07-13 16:49:36 +0900

    Python_Day_2

--- 220712/02.길이_구하기.py
+++ 220712/02.길이_구하기.py
@@ -1,10 +1,3 @@
-word = 'happy!'
-z = 0
-for x in word:
-    z += 1
-print(z)
-
-
 # 문자열 word의 길이를 출력하는 코드를 각각
 # word = 'happy!'
 # 6
@@ -14,5 +7,13 @@ count = 0
 # 모든 문자를 돌면서
 for char in word:
 # 1씩 증가시킨다.
+    count = count + 1
+print(count)
+
+
+word = 'happy!'
+count = 0
+
+for char in word:
     count = count + 1
 print(count)
