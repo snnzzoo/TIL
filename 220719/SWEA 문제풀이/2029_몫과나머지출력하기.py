fcef0be71b1aa7924d79568412ab1e15ca398f24
snnzzoo <tjss1580@gmail.com>
2022-07-20 04:03:44 +0900

    Python_Day_7

---
+++ 220719/SWEA 문제풀이/2029_몫과나머지출력하기.py
@@ -0,0 +1,8 @@
+import sys
+
+sys.stdin = open("SWEA 문제풀이/input_2029.txt", "r")
+
+t = int(input())
+for i in range(1, t+1):
+    a, b = map(int, input().split())
+    print(f'#{i} {a//b} {a%b}')
---
+++ 220719/SWEA 문제풀이/2070_큰놈,작은놈,같은놈.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('SWEA 문제풀이/input_2070.txt', 'r')
+
+T = int(input())
+for test_case in range(1, T+1):
+    a, b = map(int, input().split())
+    if a < b:
+        print(f'#{test_case} <')
+    elif a > b:
+        print(f'#{test_case} >')
+    else:
+        print(f'#{test_case} =')
