ab1518bd66100f0162b4c1c9dc47b8bb6a4ff0e9
snnzzoo <tjss1580@gmail.com>
2022-07-20 15:29:03 +0900

    SWEA 문제풀이

--- 220719/SWEA 문제풀이/2029_몫과나머지출력하기.py
+++ 220719/SWEA 문제풀이/2029_몫과나머지출력하기.py
@@ -1,10 +1,5 @@
 import sys
-
-<<<<<<< HEAD
-sys.stdin = open("220719/SWEA 문제풀이/input_2029.txt", "r")
-=======
-sys.stdin = open("SWEA 문제풀이/input_2029.txt", "r")
->>>>>>> aa84d6f97ac5eca7207ca6a9f844f4e7f009a8d7
+sys.stdin = open("2029_input.txt", "r")
 
 t = int(input())
 for i in range(1, t+1):
--- 220719/SWEA 문제풀이/2071_평균값구하기.py
+++ 220719/SWEA 문제풀이/2071_평균값구하기.py
@@ -1,9 +1,5 @@
 import sys
-<<<<<<< HEAD
-sys.stdin = open('220719/SWEA 문제풀이/input_2071.txt', 'r')
-=======
-sys.stdin = open('SWEA 문제풀이/input_2071.txt', 'r')
->>>>>>> aa84d6f97ac5eca7207ca6a9f844f4e7f009a8d7
+sys.stdin = open('2071_input.txt', 'r')
 
 T = int(input())
 for test_case in range(1, T + 1):
