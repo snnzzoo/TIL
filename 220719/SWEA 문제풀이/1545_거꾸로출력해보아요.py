84bbfee5fc936b1bde147497875d78f35fa103c2
Sunjoo IM <tjss1580@gmail.com>
2022-07-23 22:57:51 +0900

    Python Day 7

--- 220719/SWEA 문제풀이/1545_거꾸로출력해보아요.py
+++
@@ -1,7 +0,0 @@
-
-import sys
-sys.stdin = open("220719/SWEA 문제풀이/1545_input.txt", "r")
-
-T = int(input())
-for test_case in range(T, -1, -1):
-    print(test_case, end=' ')
--- 220719/SWEA 문제풀이/2029_몫과나머지출력하기.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdin = open("220719/SWEA 문제풀이/2029_input.txt", "r")
-
-t = int(input())
-for i in range(1, t+1):
-    a, b = map(int, input().split())
-    print(f'#{i} {a//b} {a%b}')
