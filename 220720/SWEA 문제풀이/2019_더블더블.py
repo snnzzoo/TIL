7a7594afcd7f5618944a7f41aa42a02f633f3436
Sunjoo IM <tjss1580@gmail.com>
2022-07-23 22:58:20 +0900

    Python Day 8

--- 220720/SWEA 문제풀이/2019_더블더블.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin = open("220720/SWEA 문제풀이/2019_input.txt", "r")
-
-N = int(input())
-result = 0
-
-for i in range(0, N + 1):
-    result = 2**i
-    print(result, end=' ')
--- 220720/SWEA 문제풀이/2050_알파벳을숫자로변환.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.stdin = open("220720/SWEA 문제풀이/2050_input.txt", "r")
-
-data = input()
-
-# ord()는 특정 문자의 아스키코드 값으로 변환
-# A는 65에 해당하므로 64를 빼 주면 된다.
-for i in data:
-    print(ord(i)-64, end=' ')
